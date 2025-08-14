aa94fd5722a49cae4eeac57d12ccc4af28aa7de8
Terje Sandstrom <terje@hermit.no>
2025-08-14 11:19:21 +0200

    removed unneeded properties, added a few functions

--- TheAnswer/YouKnow.cs
+++ TheAnswer/YouKnow.cs
@@ -1,12 +1,30 @@
 ﻿namespace WrapThat;
 
+/// <summary>
+/// A static class that provides the answer to the ultimate question of life, the universe, and everything.
+/// </summary>
 public static class YouKnow
 {
+    /// <summary>
+    /// The answer to the ultimate question of life, the universe, and everything.
+    /// </summary>
     public static int TheAnswer => 42;
+
+    /// <summary>
+    /// A static constant that represents the answer to the ultimate question of life, the universe, and everything.
+    /// </summary>
     public static int FortyTwo => TheAnswer;
 
+    /// <summary>
+    /// Returns the answer to the ultimate question of life, the universe, and everything.
+    /// </summary>
     public static int GiveMeTheAnswer()
     {
         return TheAnswer;
     }
+
+    /// <summary>
+    /// Returns a string that advises not to panic.
+    /// </summary>
+    public static string DontPanic() => "Don't Panic!";
 }
